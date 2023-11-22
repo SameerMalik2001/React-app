36b715494a6fd56a9950af86d884b6016a666501
Sameer Malik <sameer7417277576@gmail.com>
2023-11-22 10:56:16 +0530

    notepad line break fix

--- note-pad-project/src/components/SingleNote.js
+++ note-pad-project/src/components/SingleNote.js
@@ -29,7 +29,7 @@ function SingleNote() {
                 <div className='rounded-3xl w-3/5 mt-5 bg-white'>
                     <p className='text-xl font-[500] mt-8 ml-8 mr-8 text-justify'><b>Title :</b> {note[0].title}</p>
                     <div className='bg-purple-500 h-[1px] w-auto ml-8 mr-8' />
-                    <p className='text-xl font-[500] mt-4 ml-8 mr-8 text-justify mb-8'><b>Description:</b> {note[0].text}</p>
+                    <p className='text-xl font-[500] mt-4 ml-8 mr-8 text-justify mb-8'><b>Description:</b> <span dangerouslySetInnerHTML={{__html:note[0].text.replace(/\n/g, '<br>')}} /> </p>
                     <div className='flex justify-center gap-3'>
                         <button onClick={() => Edit()} className='w-28 h-11 text-white rounded-2xl bg-purple-500 text-xl mb-5 font-[500] hover:bg-purple-400'>EDIT</button>
                         <button onClick={() => sendForDelete()} className='w-28 h-11 text-white rounded-2xl bg-purple-500 text-xl mb-5 font-[500] hover:bg-purple-400'>DELETE</button>
